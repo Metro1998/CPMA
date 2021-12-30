0ce612e7d85d6ba63c1643704dc8dcf9aacf78c3
Metro <3170101740@zju.edu.cn>
2021-12-30 11:24:02 +0800

    complete 0.5

--- cpma.py
+++ cpma.py
@@ -2,6 +2,7 @@
 # @date 2021/12/29
 
 import numpy as np
+import scipy
 
 
 class CPMA(object):
@@ -19,7 +20,8 @@ class CPMA(object):
         :param pp_matrix: Propagation path Markov matrix in which state '0' is eliminated. Cause what we concern is
                           the head of the congestion propagation which should not interrupt(the cessation of congestion)
                           until the end of the propagation path(destination road segment).
-        :return:
+        :return prob: Numpy.array. prob[k]denotes the probability that the head of congestion propagates from
+                      road_segment_1 to road_segment_k+2
         """
         assert pp_matrix.shape[0] == pp_matrix.shape[-1], 'Propagation path matrix is not a square'
         num_road_segments = pp_matrix.shape[0]
@@ -28,8 +30,40 @@ class CPMA(object):
 
         return prob
 
-    def cal_expected_propagation_time(self):
+    def cal_expected_propagation_time(self, pp_matrix: np.array):
         """
 
+        :param pp_matrix:
         :return:
         """
+        assert pp_matrix.shape[0] == pp_matrix.shape[-1], 'Propagation path matrix is not a square'
+        num_road_segments = pp_matrix.shape[0]
+
+        prob = self.cal_propagation_prob(pp_matrix)
+
+        def cal_numerator(r):
+            assert r >= 2, "r must be larger than or equal to 2."
+            assert r <= num_road_segments, "r can't be larger than num_road_segments(K)"
+            onehot_left = np.eye(r - 1, dtype=int)[0]
+            onehot_right = np.transpose(np.eye(r - 1, dtype=int)[-1])
+            sub_matrix = pp_matrix[:-(num_road_segments-r+1), :-(num_road_segments-r+1)]
+            identity = np.identity(r-1)
+            tem = np.linalg.inv(identity - sub_matrix)
+            numerator = onehot_left @ tem @ tem @ onehot_right * pp_matrix[r-2][r-1]
+
+            return numerator
+        numerator_ = np.array([cal_numerator(r) for r in range(2, num_road_segments + 1, 1)])
+        assert numerator_.shape[0] == prob.shape[0], "The numerator and dominator should have the same length."
+        expected_time = numerator_ / prob
+
+        return expected_time
+
+
+
+
+
+
+
+
+
+
