a2cb0b86ff4c7b80bc1fbb5fdde74fc259d1fe4a
Metro <3170101740@zju.edu.cn>
2022-01-12 21:07:35 +0800

    complete 0.5

--- utilities.py
+++ utilities.py
@@ -5,70 +5,45 @@
 import heapq
 import os
 import numpy as np
+import pandas as pd
 
 
-class Graph:
-
-    def __init__(self, adjacent_matrix, threshold, save_path):
-        self.vertices = adjacent_matrix
-        self.size_matrix = adjacent_matrix.shape[0]
-        self.threshold = threshold  # We set a threshold to control how long the shortest path will be.
-        self.save_path = save_path  # Str.
-
-    def neighbors(self, vertex):
-        neighbors = [i for i, x in enumerate(self.vertices[vertex]) if x != 0]
-
-        return neighbors
-
-    def shortest_path(self, origin):
-        distances = [0] * self.size_matrix  # Distance from origin to node, here we use vertex index as node
-        previous = [None] * self.size_matrix  # Previous nodes in optimal path from origin
-        nodes = []  # Priority queue of all nodes in Graph
-
-        # Initialization
-        for vertex in range(self.size_matrix):
-            if vertex == origin:  # Set root node as distance of 0
-                distances[vertex] = 0
-                heapq.heappush(nodes, [0, vertex])  # TODO
+def convert_rid_2_idx(rid, segment_dict):
+    idx = []
+    for i in range(len(rid)):
+        temp_list = []
+        for j in range(len(rid[i])):
+            if rid[i][j] != 0:
+                temp_list.append(segment_dict[rid[i][j]])
             else:
-                distances[vertex] = float('inf')  # Set distance to 2^32-1 in default
-                heapq.heappush(nodes, [float('inf'), vertex])
-
-        while nodes:
-            smallest = heapq.heappop(nodes)[1]  # Vertex in nodes with smallest distance, and remove if from heapq
-            if distances[smallest] == float('inf'):
-                print('All remaining vertices are inaccessible from origin')
                 break
+        idx.append(list(set(temp_list)))
+    return idx
 
-            for neighbor in self.neighbors(smallest):  # Look at all the nodes that this vertex is attached to
-                alt = distances[smallest] + self.vertices[smallest][neighbor]  # Alternative path distance
-                if alt < distances[neighbor]:  # If there is a new shortest path, update our priority queue(relax)
-                    if alt > self.threshold:
-                        print('Break in advance.')
-                        break
-                    distances[neighbor] = alt
-                    previous[neighbor] = smallest
-                    for n in nodes:
-                        if n[1] == neighbor:
-                            n[0] = alt
-                            break
-                    heapq.heapify(nodes)
-        return distances
 
-    def __str__(self):
-        return str(self.vertices)
+def preprocess():
+    path = './nostop_speed_SRI/'
+    segment = np.array(pd.read_csv(path + 'segment_csv')).squeeze()
+    idx = np.arange(len(segment))
+    segment_dict = dict(zip(segment, idx))
 
-    def update_shortest_path_matrix(self):
-        shortest_path_matrix = []
-        for vertex in range(self.size_matrix):
-            distance = self.shortest_path(vertex)
-            shortest_path_matrix.append(distance)
-        shortest_path_matrix = np.array(shortest_path_matrix)
-        # if os.path.exists('../data/shortest_path_matrix.npy'):
-        #     os.remove('../data/shortest_path_matrix.npy')
-        #     print('Delete successfully.')
+    longs = np.array(pd.read_csv(path + "longs.csv"))
+    lats = np.array(pd.read_csv(path + "lats.csv"))
 
-        return shortest_path_matrix
+    children = np.array(pd.read_csv(path + "children.csv").fillna(0))
+    children = convert_rid_2_idx(children, segment_dict=segment_dict)
+    children = np.array(children)
+
+    parents = np.array(pd.read_csv(path + "parents.csv").fillna(0))
+    parents = convert_rid_2_idx(parents, segment_dict)
+    parents = np.array(parents)
+
+    longer_pairs = np.array(pd.read_csv(path + "congestion_segment.csv").fillna(0))
+    longer_pairs = convert_rid_2_idx(longer_pairs, segment_dict)
+    longer_pairs = np.array(longer_pairs)
+    time_removed = np.array(pd.read_csv(path + "time_removed.csv"))
+
+
 
 
 if __name__ == '__main__':
